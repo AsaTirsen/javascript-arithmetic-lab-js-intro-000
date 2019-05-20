a1516020d26b1ed587a641e81a32819e7357bed7
Asa Tirsen <asa.tirsen@gmail.com>
2019-05-20 03:41:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+// function for addition
+function add(a, b) {
+  return a+b;
+}
+
+// function for subtraction
+function subtract(a, b) {
+  return a-b;
+}
+
+// function for multiplication
+function multiply(a, b) {
+  return a*b;
+}
+
+// function for multiplication
+function divide(a, b) {
+  return a/b;
+}
+
+//increments n
+function inc(n) {
+  return ++n;
+}
+
+//decrements n
+function dec(n) {
+  return --n;
+}
